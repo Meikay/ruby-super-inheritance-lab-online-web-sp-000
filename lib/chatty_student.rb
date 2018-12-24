4c1159694642e95bff85baa8e91ecfac69781c25
Meisuleen Kased <meisuleen@gmail.com>
2018-12-24 03:58:53 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,10 +2,7 @@ class ChattyStudent < Student
 
   def hello
     super
-    @in_class = true
-    puts "Hey there! I'm so excited to learn stuff.\nHow are you doi
-     ng today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any s
-     poilers? Okay well let me just tell you who died...\n"
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 
 
