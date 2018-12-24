71e4681afabaaa9ca912e94051df23b3b16daca6
Meisuleen Kased <meisuleen@gmail.com>
2018-12-24 03:53:04 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -5,7 +5,7 @@ class ChattyStudent < Student
   # end
 
   def hello
-    "Hey there! I'm so excited to learn stuff.\nHow are you doi
+    puts "Hey there! I'm so excited to learn stuff.\nHow are you doi
      ng today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any s
      poilers? Okay well let me just tell you who died...\n"
   end
