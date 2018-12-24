d70f57890b6fe7778980e01ee19f2413d3900a88
Meisuleen Kased <meisuleen@gmail.com>
2018-12-24 03:57:17 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,6 +2,7 @@ class ChattyStudent < Student
 
   def hello
     super
+    @in_class = true
     puts "Hey there! I'm so excited to learn stuff.\nHow are you doi
      ng today? I'm okay, but I'm kind of tired....n, it was so crazy! What, you don't want any s
      poilers? Okay well let me just tell you who died...\n"
